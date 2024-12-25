54990cbe18d22cd8a26ef958b6712a5937e3d61a
Ayush Kohre <kohreaayush@gmail.com>
2024-12-26 03:24:15 +0530

    Module - 5(React) | Day - 2 Assignment

---
+++ Module_5/React/ClassWork/CW2/app.js
@@ -0,0 +1,48 @@
+document.body.style.backgroundColor = "#9e1b32";
+
+const heading = React.createElement(
+  "h1",
+  {
+    style: {
+      backgroundColor: "#FBCEB1",
+      color: "#9e1b32",
+      textTransform: "uppercase",
+      textAlign: "center",
+      fontSize: "3.44rem",
+      letterSpacing: ".37em",
+    },
+  },
+  "Welcome to React using CDN!"
+);
+
+const paragraph = React.createElement(
+  "p",
+  {
+    style: {
+      textAlign: "center",
+      fontSize: "2.6em",
+      color: "#FF7F50",
+      letterSpacing: ".052em",
+    },
+  },
+  "This is my 1st simple task: I completed using React and ReactDOM from CDN."
+);
+
+const list = React.createElement(
+  "ul",
+  {
+    style: {
+      listStyleType: "none",
+      textAlign: "center",
+      color: "#FFA07A",
+      fontSize: "3rem",
+    },
+  },
+  React.createElement("li", null, "Bg-Color: Bills Red"),
+  React.createElement("li", null, "HeadingBgColor: Apricot"),
+  React.createElement("li", null, "ParagraphColor: Coquelicot")
+);
+
+const app = React.createElement("div", null, heading, paragraph, list);
+
+ReactDOM.render(app, document.getElementById("root"));
