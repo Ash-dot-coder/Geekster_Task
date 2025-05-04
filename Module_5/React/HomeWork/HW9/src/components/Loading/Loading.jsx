bfa18f2ca570440fd4058cf9d8322cd9bcc5e4db
Ayush Kohre <kohreaayush@gmail.com>
2025-05-04 23:29:52 +0530

    HW9 - Quiz

---
+++ Module_5/React/HomeWork/HW9/src/components/Loading/Loading.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Loading.css';
+
+const Loading = () => {
+  return (
+    <div className="loading-wrapper">
+        <div className='loadership_QXDOL'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <h2>Loading...</h2>
+    </div>
+  );
+}
+
+export default Loading;
