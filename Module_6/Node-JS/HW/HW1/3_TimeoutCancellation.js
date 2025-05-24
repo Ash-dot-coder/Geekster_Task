ed7ddaa28ca1c9bea1c44817ec1af31ac6cea515
Ayush Kohre <kohreaayush@gmail.com>
2025-05-24 17:43:34 +0530

    ClassWork : Three-Tier Architecture

--- Module_6/Node-JS/HW/HW1/3_TimeoutCancellation.js
+++
@@ -1,9 +0,0 @@
-var cancellable = function (fn, args, t) {
-  const timeoutId = setTimeout(() => {
-    fn(...args);
-  }, t);
-
-  return function cancelFn() {
-    clearTimeout(timeoutId);
-  };
-};
--- Module_6/Node-JS/HW/HW1/4_AddTwoPromises.js
+++
@@ -1,3 +0,0 @@
-var addTwoPromises = function (promise1, promise2) {
-  return Promise.all([promise1, promise2]).then(([val1, val2]) => val1 + val2);
-};
